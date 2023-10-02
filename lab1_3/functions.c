8bfee6c940aee1438e4d71244970b2002653981e
ykwais <140707281+ykwais@users.noreply.github.com>
2023-10-02 20:50:52 +0300

    third laba

--- lab1_3/functions.c
+++ lab1_3/functions.c
@@ -1,9 +1,12 @@
-//
-// Created by fedor on 28.09.2023.
-//
 
-#ifndef LABORATORY1_3_FUNCS_H
-#define LABORATORY1_3_FUNCS_H
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdbool.h>
+
+#include "enums.h"
+
 bool is_number(char* str){
     char* result ;
     long double number = strtold(str, &result);
@@ -12,6 +15,67 @@ bool is_number(char* str){
     }
     return true;
 }
+
+enum input_status_code analysis(int argc, char** argv){
+    if(argc == 2 && (!strcmp(argv[1], "-help") || !strcmp(argv[1],"/help") )){
+        return isc_need_help;
+    }
+    else if(argc == 6 && (!strcmp(argv[1], "-q") || !strcmp(argv[1],"/q") ) && is_number(argv[2]) && is_number(argv[3]) && is_number(argv[4]) && is_number(argv[5])){
+        return isc_q;
+    }
+    else if(argc == 4 && (!strcmp(argv[1], "-m") || !strcmp(argv[1],"/m") ) && is_number(argv[2]) && is_number(argv[3])){
+        return isc_m;
+    }
+    else if(argc == 6 && (!strcmp(argv[1], "-t") || !strcmp(argv[1],"/t") ) && is_number(argv[2]) && is_number(argv[3]) && is_number(argv[4]) && is_number(argv[5]) ){
+        return isc_t;
+    }
+    else{
+        return isc_invalid_parametr;
+    }
+}
+
+enum m_status_code is_divider(long long first_number, long long second_number){
+    if(first_number % second_number == 0){
+        return m_yes;
+    }
+    else return m_no;
+}
+
+enum t_status_code is_triangle(char** argv){
+    char* end;
+    long double eps = strtold(argv[2], &end);
+    long double a = strtold(argv[3], &end);
+    long double b = strtold(argv[4], &end);
+    long double c = strtold(argv[5], &end);
+    if( eps < 0 || a < 0 || b < 0 || c < 0 || fabsf(a) < eps || fabsl(b) < eps || fabsl(c) < eps){
+        return tsc_bad_input;
+    }
+    else if( c-a-b <= 0 && b-c-a <= 0 && a-b-c <= 0 && (fabsl(c*c - a*a - b*b) <= eps || fabsl(a*a - b*b - c*c) <= eps || fabsl(b*b - a*a - c*c) <= eps) ){
+        return tsc_normal;
+    }
+    else{
+        return tsc_soso;
+    }
+
+}
+
+enum q_status_code is_triple(char** argv){
+    char* end;
+    long double eps = strtold(argv[2], &end);
+    long double a = strtold(argv[3], &end);
+    long double b = strtold(argv[4], &end);
+    long double c = strtold(argv[5], &end);
+    if((fabsl(a) <= eps && fabsl(b) <= eps && fabsl(c) <= eps)){
+        return qsc_three_zero;
+    }
+    else if(fabsl(a-b)<=eps || fabsl(a-c) <= eps || fabsl(b-c) <= eps){
+        return qsc_half_full;
+    }
+    else{
+        return qsc_full;
+    }
+}
+
 
 void solution(long double eps, long double a, long double b, long double c){
     long double sol_1, sol_2;
@@ -101,5 +165,3 @@ void not_full_cicle(long double epsilon, long double* mass){
 
 
 }
-
-#endif //LABORATORY1_3_FUNCS_H
