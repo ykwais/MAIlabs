d0437eab4d370a88f4973882288b67e460650dfb
ykwais <140707281+ykwais@users.noreply.github.com>
2023-10-02 20:45:10 +0300

    second laba

--- lab1_2/enums.h
+++ lab1_2/enums.h
@@ -1,6 +1,4 @@
-//
-// Created by fedor on 30.09.2023.
-//
+
 
 #ifndef LABORATORY1_2_ENUMS_H
 #define LABORATORY1_2_ENUMS_H
@@ -10,16 +8,4 @@ enum input_status_code{
     isc_normal
 };
 
-
-enum input_status_code analysis(int argc, char** argv){
-    if(argc == 2 && (!strcmp(argv[1], "-help") || !strcmp(argv[1],"/help") )){
-        return isc_need_help;
-    }
-    else if(argc == 2 && is_number(argv[1]) && argv[1][0] >= 48 && argv[1][0] < 56 && argv[1][1] == '\0' ){
-        return isc_normal;
-    }
-    else{
-        return isc_invalid_parametr;
-    }
-}
 #endif //LABORATORY1_2_ENUMS_H
