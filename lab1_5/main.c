34b479f691a29e47b7d134fd231001d9c656b6f9
ykwais <140707281+ykwais@users.noreply.github.com>
2023-10-03 19:28:25 +0300

    update laba1_5

--- lab1_5/main.c
+++ lab1_5/main.c
@@ -24,8 +24,8 @@ int main(int argc, char **argv) {
             printf("you have entered x greater than 1, so you will be able to calculate only the first two sums. x < 1 and x > -1!!!\n");
             printf("First summ: %.20LF\n", first_summ(strtold(argv[1], NULL), strtold(argv[2], NULL)));
             printf("Second summ: %.20LF\n", second_summ(strtold(argv[1], NULL), strtold(argv[2], NULL)));
-            printf("Third summ: ----\n");
-            printf("Fourth summ: ----\n");
+            printf("Third summ: diverges\n");
+            printf("Fourth summ: diverges\n");
             break;
         case isc_well_x:
             printf("well:\n");
