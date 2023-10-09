0b0ce08d5f8928115fc60896e330367abfbc9271
ykwais <140707281+ykwais@users.noreply.github.com>
2023-10-09 03:06:53 +0300

    Update funcs.c

--- lab1_8/funcs.c
+++ lab1_8/funcs.c
@@ -5,8 +5,7 @@
 #include <ctype.h>
 #include "enums.h"
 
-#define MAX_LEN 65
-#define SIZE 65
+
 
 void skip_the_voids(FILE *file_name) {
     int ch;
@@ -18,45 +17,91 @@ void skip_the_voids(FILE *file_name) {
     }
 }
 
-char *collect_a_lexemma(FILE *file_name) {
-    char *lexemma = (char *) malloc(sizeof(char) * MAX_LEN);
+char *collect_a_lexemma(FILE *file_name, enum troubles_with_lexemma* stat) {
+    int length = 2;
+    char *lexemma = (char *) malloc(sizeof(char) * length);
     if (lexemma == NULL) {
         return NULL;
     }
     int ch;
-    int length_lexemma = 0;
+
+    int index = 0;
+    char *tmp = NULL;
+
     while ((ch = fgetc(file_name)) != EOF) {
         if (ch == 10 || ch == 9 || ch == 32) {
             break;
         }
-        if (length_lexemma < MAX_LEN) {
-            lexemma[length_lexemma] = (char) ch;
-            length_lexemma++;
-        } else {
+        if (length <= index)
+        {
+            length <<= 1;
 
-            return NULL;
+            if (!(tmp = (char*) realloc(lexemma, sizeof (char) * length)))
+            {
+                free(lexemma);
+                *stat = twl_troubles;
+                return NULL;
+            }
+            else
+            {
+                lexemma = tmp;
+            }
         }
+
+        lexemma[index++] = ch;
     }
-    lexemma[length_lexemma] = '\0';
+
+    if (!(tmp = (char*) realloc(lexemma, sizeof (char) * (index + 1))))
+    {
+        free(lexemma);
+        *stat = twl_troubles;
+        return NULL;
+    }
+    else
+    {
+        lexemma = tmp;
+    }
+    lexemma[index] = '\0';
     return lexemma;
 }
 
-long long string_cc_to_10CC(int base, char *string) {
+int length_long_long(long long number){
+    char random_string[20];
+    sprintf(random_string, "%lld",number);
+
+    return (int)strlen(random_string);
+}
+
+long long string_cc_to_10CC(int base, char *string, enum overflow* status_overflow) {//здесь буду отслеживать переполнение
+    bool negative = false;
     char *ptr = string;
     long long result = 0;
     while (*ptr) {
+        if(*ptr == 45){
+            negative = true;
+            ptr++;
+            continue;
+        }
+        if(length_long_long(result) > 17 ){
+            *status_overflow = overflow;
+            break;
+        }
         result = result * base + (isdigit(*ptr) ? *ptr - '0' : *ptr - 'A' + 10);
         ptr++;
     }
 
-
+    if(negative)
+    {
+        return -1*result;
+    }
     return result;
 
 }
 
 void divided_string(char *arg, FILE *output) {
-    char *tmp = (char *) malloc(sizeof(char) * strlen(arg));
     int length = strlen(arg);
+    char *tmp = (char *) malloc(sizeof(char) * length);
+
     int counter = 0;
     int counter_2 = 0;
     int p = 0;
@@ -101,11 +146,12 @@ void divided_string(char *arg, FILE *output) {
 }
 
 char *to_upper_text(char *string) {
-    char *tmp = (char *) malloc(sizeof(char) * strlen(string));
+    int length = strlen(string);
+    char *tmp = (char *) malloc(sizeof(char) * (length+1));
     if (tmp == NULL) {
         return NULL;
     }
-    int length = strlen(string);
+
     for (int i = 0; i < length; i++) {
         tmp[i] = toupper(string[i]);
     }
@@ -119,7 +165,7 @@ int min_cc(char *string) {
     for (int i = 0; i < length; i++) {
         if (isdigit(string[i]) && string[i] - '0' >= max) {
             max = string[i] - '0';
-        } else if (isupper(string[i]) && string[i] - 'A' + 10 >= max) {
+        } else if (isupper(string[i]) && string[i] - 'A' + 10 >= max ) {
             max = string[i] - 'A' + 10;
         } else {
             continue;
@@ -134,44 +180,64 @@ int min_cc(char *string) {
 bool is_number(char *string) {
 
     int length = strlen(string);
+
     for (int i = 0; i < length; i++) {
         int x = string[i];
-        if (x < 48 || (x > 57 && x < 65) || (x > 90 && x < 97) || x > 122) {
+        if (!isalnum(x) && !(string[i] == '-' && i == 0)) {
             return false;
         }
+        if(length == 1 && string[i] == '-'){
+            return false;
+        }
+
     }
     return true;
 }
 
 
-void do_flag(FILE *input, FILE *output, enum flag_status_code *is_num) {
+void do_flag(FILE *input, FILE *output, enum flag_status_code *is_num, enum overflow* status_overflow) {
     skip_the_voids(input);
+    enum troubles_with_lexemma status_trouble_lexemma = twl_no_troubles;
     while (!feof(input)) {
-        char *lexemma = collect_a_lexemma(input);
+        char *lexemma = collect_a_lexemma(input, &status_trouble_lexemma);
+
         if (!is_number(lexemma)) {
+            //freopen(output, "w+", NULL);
+            //fclose(output);
             *is_num = not_a_number;
             break;
         }
-        if (lexemma == NULL) {
-            fprintf(output, "(too_long_string) ");
-            free(lexemma);
+        if (lexemma == NULL && status_trouble_lexemma == twl_troubles) {
+            status_trouble_lexemma = twl_no_troubles;
+            printf("");
             skip_the_voids(input);
             lexemma = NULL;
         } else {
-            char *upper_lexemma = to_upper_text(lexemma);
+            char* upper_lexemma = to_upper_text(lexemma);
             divided_string(upper_lexemma, output);
-            //fprintf(output, "%s ", upper_lexemma);
+
             int minim_cc = min_cc(upper_lexemma);
+            *status_overflow = not_overflow;
+            long long number_in_10cc = string_cc_to_10CC(minim_cc, upper_lexemma, status_overflow);
             fprintf(output, "%d ", minim_cc);
-            fprintf(output, "%lld\n", string_cc_to_10CC(minim_cc, upper_lexemma));
+            if(*status_overflow == overflow){
+                fprintf(output, "Overflow\n");
+                *status_overflow = not_overflow;
+            }else {
+                fprintf(output, "%lld\n", number_in_10cc);
+            }
+
+
 
             free(lexemma);
-            lexemma = NULL;
+
             free(upper_lexemma);
-            upper_lexemma = NULL;
+
+
             skip_the_voids(input);
         }
     }
+
 }
 
 enum flag_status_code can_flag(char *input_file_name, char *output_file_name) {
@@ -179,10 +245,15 @@ enum flag_status_code can_flag(char *input_file_name, char *output_file_name) {
     FILE *input_file = fopen(input_file_name, "r");
 
     if (!output_file || !input_file) {
+        fclose(input_file);
+
+        fclose(output_file);
         return file_open_problem;
     } else {
         enum flag_status_code is_num;
-        do_flag(input_file, output_file, &is_num);
+        enum overflow sts;
+        do_flag(input_file, output_file, &is_num, &sts);
+
         if (is_num == not_a_number) {
             return not_a_number;
         }
@@ -195,8 +266,8 @@ enum flag_status_code can_flag(char *input_file_name, char *output_file_name) {
 }
 
 enum input_status_code analysis(int argc, char** argv){
-    if(argc < 2 || argc > 4){
-        return isc_little_argc;
+    if(argc != 3){
+        return isc_wrong_argc;
     }
 
     else{
